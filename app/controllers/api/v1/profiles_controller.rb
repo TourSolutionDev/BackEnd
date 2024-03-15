2f2ea3303eea5bf15aa5a971ca502ed351e251fc
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-15 06:43:59 +0300

    Fix profiles controller and routes

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,41 +1,62 @@
 class Api::V1::ProfilesController < ApplicationController
-  before_action :set_profile, only: %i[ show update destroy ]
-
-  # GET /profiles
+  # before_action :set_profile, only: %i[ show update destroy ]
+  before_action :authenticate_api_v1_user!, only: %i[ show create update destroy ]
+  before_action :authenticate_api_v1_admin!, only: %i[ index ]
+  # GET /api/v1/profiles
   def index
     @profiles = Profile.all
 
     render json: @profiles
   end
 
-  # GET /profiles/1
+  # GET /api/v1/profiles/
   def show
-    render json: @profile
-  end
-
-  # POST /profiles
-  def create
-    @profile = Profile.new(profile_params)
-
-    if @profile.save
-      render json: @profile, status: :created, location: @profile
-    else
-      render json: @profile.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /profiles/1
-  def update
-    if @profile.update(profile_params)
+    @profile = current_api_v1_user.profile
+    if @profile
       render json: @profile
     else
+      render json: { success: false , message: 'Profile not found' }, status: :not_found
+    end
+    #render json: @profile, location: api_v1_profile_url(@profile)
+  end
+
+  # POST /api/v1/profiles/
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_api_v1_user
+
+    if @profile.save
+      render json: @profile, status: :created, location: api_v1_profile_url(@profile)
+    else
       render json: @profile.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /profiles/1
+
+  # PATCH/PUT /api/v1/profile
+  def update
+    @profile = current_api_v1_user.profile
+    if @profile
+      if @profile.update(profile_params)
+        render json: @profile
+      else
+        render json: @profile.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { success: false , message: 'Profile not found' }, status: :not_found
+    end
+  end
+
+  # DELETE /api/v1/profiles
   def destroy
-    @profile.destroy!
+    @profile = current_api_v1_user.profile
+    if @profile
+      @profile.destroy!
+      render json: { success: true, message: 'Profile deleted successfully' }
+    else
+      render json: { success: false , message: 'Profile not found'}, status: :not_found
+
+    end
   end
 
   private
@@ -46,6 +67,11 @@ class Api::V1::ProfilesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :phone_number, :passport, :profile_picture, :user_id)
+      params.require(:profile).permit(:first_name, :last_name, :phone_number, :passport, :profile_picture)
     end
+
+    # def profile_params
+    #   params.require(:profile).permit(:first_name, :last_name, :phone_number, :passport, :profile_picture)
+    # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :profiles
+      #resource :profiles # it will create all the routes for profiles like index, show, create, update, destroy
+      # the index route will be like this: /api/v1/profiles
+
+      # get 'profiles', to: 'profiles#index'
+      resources :profiles, only: [:index] # GET /api/v1/profiles
+      resource :profile, only: [:show, :create, :update, :destroy] # GET, POST, PATCH/PUT, DELETE /api/v1/profile
 
 
       mount_devise_token_auth_for 'User', at: 'auth'
