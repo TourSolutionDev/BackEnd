3da54d4024918815c79b44ec933f787f4157b4e6
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-17 19:28:11 +0300

    Add association for destinations

--- app/models/api/v1/tour.rb
+++ app/models/api/v1/tour.rb
@@ -3,6 +3,7 @@ class Api::V1::Tour < ApplicationRecord
   belongs_to :admin
   has_many :tour_dates
   has_many :prices
+  has_many :destionations
   validates :name, :description, :duration, presence: true
   validates :name, uniqueness: true
   validates :duration, numericality: { only_integer: true, greater_than: 0 }
