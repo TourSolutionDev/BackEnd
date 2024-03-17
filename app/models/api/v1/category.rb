8338abaf4cbe8935d3abd8a79690f6a3e6660394
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-17 09:06:29 +0300

    Add associations and validations to models

--- app/models/api/v1/category.rb
+++ app/models/api/v1/category.rb
@@ -1,3 +1,4 @@
 class Api::V1::Category < ApplicationRecord
   belongs_to :admin
+  has_many :tours
 end
--- app/models/api/v1/tour.rb
+++ app/models/api/v1/tour.rb
@@ -1,4 +1,8 @@
 class Api::V1::Tour < ApplicationRecord
   belongs_to :category
   belongs_to :admin
+  validates :name, :description, :duration, presence: true
+  validates :name, uniqueness: true
+  validates :duration, numericality: { only_integer: true, greater_than: 0 }
+
 end
