edbc45318afefe3b8d582207b55c37dc75b1ca80
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-17 05:14:54 +0300

    Add Api V1 module, generate category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_222734) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_17_020502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_14_222734) do
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_admins_on_uid_and_provider", unique: true
+  end
+
+  create_table "api_v1_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
---
+++ spec/factories/api/v1/categories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :api_v1_category, class: 'Api::V1::Category' do
+    name { "MyString" }
+  end
+end
