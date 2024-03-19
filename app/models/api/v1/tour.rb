f143832ad654cebad5d3055fe0990dfb2b90f30d
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-19 09:09:10 +0300

    change associations and add nested attributes

--- app/models/api/v1/tour.rb
+++ app/models/api/v1/tour.rb
@@ -1,10 +1,14 @@
 class Api::V1::Tour < ApplicationRecord
   belongs_to :category
   belongs_to :admin
-  has_many :tour_dates
-  has_many :prices
-  has_many :destionations
-  has_many :activities
+  has_many :tour_dates , dependent: :destroy
+  has_many :prices , dependent: :destroy
+  has_many :destinations , dependent: :destroy
+  has_many :activities , dependent: :destroy
+  accepts_nested_attributes_for :tour_dates, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :prices, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :destinations, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :activities, allow_destroy: true,  reject_if: :all_blank
   validates :name, :description, :duration, presence: true
   validates :name, uniqueness: true
   validates :duration, numericality: { only_integer: true, greater_than: 0 }
