63df49a4e3c0d1aff8c278146d04fa6a6ed34e61
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-19 09:09:57 +0300

    Refactor tour factory in

--- spec/factories/api/v1/tours.rb
+++ spec/factories/api/v1/tours.rb
@@ -4,22 +4,23 @@ FactoryBot.define do
     description { Faker::Lorem.paragraph }
     duration { Faker::Number.within(range: 1..10) }
     image { Faker::Avatar.image }
-    category { create(:api_v1_category)}
-    admin { create(:admin)}
+    category { build(:api_v1_category)}
+    admin { build(:admin)}
 
-    transient do
-      tour_dates_count { 2 }
-      prices_count { 2 }
-      destinations_count { 2 }
-      activities_count { 2 }
-    end
 
-    after(:build) do |tour, evaluator|
-      tour.tour_dates << build_list(:api_v1_tour_date, evaluator.tour_dates_count, tour: tour)
-      tour.prices << build_list(:api_v1_price, evaluator.prices_count, tour: tour)
-      tour.destinations << build_list(:api_v1_destination, evaluator.destinations_count, tour: tour)
-      tour.activities << build_list(:api_v1_activity, evaluator.activities_count, tour: tour)
-    end
+    # transient do
+    #   tour_dates_count { 2 }
+    #   prices_count { 2 }
+    #   destinations_count { 2 }
+    #   activities_count { 2 }
+    # end
+
+    # after(:build) do |tour, evaluator|
+    #   tour.tour_dates << build_list(:api_v1_tour_date, evaluator.tour_dates_count, tour: tour)
+    #   tour.prices << build_list(:api_v1_price, evaluator.prices_count, tour: tour)
+    #   tour.destinations << build_list(:api_v1_destination, evaluator.destinations_count, tour: tour)
+    #   tour.activities << build_list(:api_v1_activity, evaluator.activities_count, tour: tour)
+    # end
 
   end
 end
