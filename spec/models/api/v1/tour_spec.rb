d48eb830af76cad71c2570bdfd405b2c19232cc8
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-19 09:03:55 +0300

    Add  test for tours

--- spec/models/api/v1/tour_spec.rb
+++ spec/models/api/v1/tour_spec.rb
@@ -29,4 +29,15 @@ RSpec.describe Api::V1::Tour, type: :model do
     it { should accept_nested_attributes_for(:destinations).allow_destroy(true) }
     it { should accept_nested_attributes_for(:activities).allow_destroy(true) }
   end
+
+# check if the factory bot is overriding attributes im passing in the test
+  describe "factory" do
+    it "is should create a tour with specified attributes" do
+      tour = create(:api_v1_tour, name: "New Tour", duration: 5, description: "New Tour Description")
+      expect(tour.name).to eq("New Tour")
+      expect(tour.duration).to eq(5)
+      expect(tour.description).to eq("New Tour Description")
+    end
+  end
+
 end
