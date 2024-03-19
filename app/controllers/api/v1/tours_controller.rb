58c867670bc2e3756c7b47c7626af96ebb571b37
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-19 09:06:31 +0300

    Add authentication for admin users and handle creation of new categories

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ToursController < ApplicationController
   before_action :set_api_v1_tour, only: %i[ show update destroy ]
+  before_action :authenticate_api_v1_admin!
 
   # GET /api/v1/tours
   def index
@@ -15,13 +16,26 @@ class Api::V1::ToursController < ApplicationController
 
   # POST /api/v1/tours
   def create
+    if params[:api_v1_tour][:category_id].present?
+    # If category_id is present, it means an existing category is selected
     @api_v1_tour = Api::V1::Tour.new(api_v1_tour_params)
+    else
+      # If category_id is not present, it means a new category is being created
+      @category = Api::V1::Category.new(category_params)
+      @category.save! #
+
+      @api_v1_tour = @category.tours.build(api_v1_tour_params)
+    end
+
+    @api_v1_tour.admin = current_api_v1_admin
+
 
     if @api_v1_tour.save
       render json: @api_v1_tour, status: :created, location: @api_v1_tour
     else
       render json: @api_v1_tour.errors, status: :unprocessable_entity
     end
+
   end
 
   # PATCH/PUT /api/v1/tours/1
@@ -46,6 +60,15 @@ class Api::V1::ToursController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def api_v1_tour_params
-      params.require(:api_v1_tour).permit(:name, :description, :duration, :image, :category_id, :admin_id)
+      params.require(:api_v1_tour).permit(
+      :name, :description, :duration, :image, :category_id,
+      tour_dates_attributes: [:id, :start_date, :end_date, :_destroy],
+      prices_attributes: [:id, :amount, :currency, :_destroy],
+      destinations_attributes: [:id, :name, :description, :_destroy],
+      activities_attributes: [:id, :name, :_destroy])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
     end
 end
