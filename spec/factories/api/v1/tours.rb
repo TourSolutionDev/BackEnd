92870981d89061b98ff278e8ccb2ff73f639e355
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-19 04:43:18 +0300

    Refactor tour factory in to include nested attributes

--- spec/factories/api/v1/tours.rb
+++ spec/factories/api/v1/tours.rb
@@ -1,10 +1,25 @@
 FactoryBot.define do
   factory :api_v1_tour, class: 'Api::V1::Tour' do
-    name { "MyString" }
-    description { "MyText" }
-    duration { 1 }
-    image { "MyString" }
-    category { nil }
-    admin { nil }
+    name { Faker::Name.unique.name }
+    description { Faker::Lorem.paragraph }
+    duration { Faker::Number.number(digits: 1) }
+    image { Faker::Avatar.image }
+    category { create(:api_v1_category)}
+    admin { create(:admin)}
+
+    transient do
+      tour_dates_count { 2 }
+      prices_count { 2 }
+      destinations_count { 2 }
+      activities_count { 2 }
+    end
+
+    after(:build) do |tour, evaluator|
+      tour.tour_dates << build_list(:api_v1_tour_date, evaluator.tour_dates_count, tour: tour)
+      tour.prices << build_list(:api_v1_price, evaluator.prices_count, tour: tour)
+      tour.destinations << build_list(:api_v1_destination, evaluator.destinations_count, tour: tour)
+      tour.activities << build_list(:api_v1_activity, evaluator.activities_count, tour: tour)
+    end
+
   end
 end
