c091d3fc09ea66c0d364c051efaf355ffac02d57
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-20 12:13:34 +0300

    Add additional attributes and associations

--- app/serializers/api/v1/tour_serializer.rb
+++ app/serializers/api/v1/tour_serializer.rb
@@ -1,6 +1,23 @@
 class Api::V1::TourSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :duration, :image
+  attributes :id, :name, :description, :duration, :image, :average_rating, :reviews_count
   has_one :category
-  has_one :admin
+  # has_one :admin
   has_many :reviews, serializer: Api::V1::ReviewSerializer
+  has_many :tour_dates, serializer: Api::V1::TourDateSerializer
+  has_many :prices, serializer: Api::V1::PriceSerializer
+  has_many :destinations, serializer: Api::V1::DestinationSerializer
+  has_many :activities, serializer: Api::V1::ActivitySerializer
+  # has_many :bookings, serializer: Api::V1::BookingSerializer
+  # has_many :users, through: :bookings
+  has_many :reviews, serializer: Api::V1::ReviewSerializer
+  # add average_rating to the json response
+  def average_rating
+    Api::V1::Review.average_rating(object.id)
+  end
+
+  def reviews_count
+    Api::V1::Review.reviews_count(object.id)
+  end
+
+
 end
