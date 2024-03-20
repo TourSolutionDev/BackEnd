6e1eaec826adb22d54b598f44c5f4bea014c0e9a
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-20 20:29:45 +0300

    Fixed amount

--- spec/factories/api/v1/prices.rb
+++ spec/factories/api/v1/prices.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :api_v1_price, class: 'Api::V1::Price' do
-    amount { Faker::Number.number(digits: 4) }
+    price { Faker::Number.decimal(l_digits: 2) }
     currency { Faker::Currency.code }
     tour { create(:api_v1_tour)}
   end
