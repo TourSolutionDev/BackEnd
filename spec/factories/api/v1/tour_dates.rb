60725b16a3b678711467af2595f706980ec78090
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-20 20:02:19 +0300

    Fix start date and end date

--- spec/factories/api/v1/tour_dates.rb
+++ spec/factories/api/v1/tour_dates.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :api_v1_tour_date, class: 'Api::V1::TourDate' do
-    start_date { Faker::Date.between(from: '2021-03-17', to: '2024-03-17') }
-    end_date { Faker::Date.between(from: start_date, to: '2024-03-17') }
+    start_date { Faker::Date.between(from: Date.today, to: 1.year.from_now)}
+    end_date { Faker::Date.between(from: start_date, to: 1.year.from_now)}
     tour { create(:api_v1_tour)}
   end
 end
