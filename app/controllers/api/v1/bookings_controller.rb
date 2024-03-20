12a210b93e1f754bf17c638f66103104f4c0036b
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-20 18:29:29 +0300

    Update authentication for canceling

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BookingsController < ApplicationController
   before_action :set_api_v1_booking, only: %i[ show update destroy ]
-  before_action :authenticate_api_v1_user!, only: %i[ create show ]
+  before_action :authenticate_api_v1_user!, only: %i[ create show cancel ]
   before_action :authenticate_api_v1_admin!, only: %i[ index update destroy ]
 
   # GET /api/v1/bookings
