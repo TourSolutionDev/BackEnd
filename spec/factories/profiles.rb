248a3c9dae43c6ac15303731c5b21018c161b731
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-20 19:26:30 +0300

    Fix phone number validation

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :profile do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    phone_number { Faker::PhoneNumber.cell_phone }
+    phone_number { Faker::Number.number(digits: rand(10..13)) }
     passport { Faker::IDNumber.valid }
     profile_picture { Faker::Avatar.image }
     association :user, factory: :user
