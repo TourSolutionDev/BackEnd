1258fad556f1ce1f1ef5b622c45b5b6252935bf1
Adoniyas7 <adoniyasg7s@gmail.com>
2024-03-20 18:23:36 +0300

    Fix request test

--- spec/requests/api/v1/bookings_spec.rb
+++ spec/requests/api/v1/bookings_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 
-
-
 RSpec.describe "/api/v1/bookings", type: :request do
-  # This should return the minimal set of attributes required to create a valid
-  # Api::V1::Booking. As you add validations to Api::V1::Booking, be sure to
-  # adjust the attributes here as well.
   let(:user) { create(:user) }
   let(:tour) { create(:api_v1_tour) }
+  let(:admin) { create(:admin) }
+
   let(:valid_attributes) {
     {
       user_id: user.id,
@@ -26,45 +23,61 @@ RSpec.describe "/api/v1/bookings", type: :request do
   }
 
   let(:user_headers) { user.create_new_auth_token }
-  let(:valid_headers) {
+  let(:admin_headers) { admin.create_new_auth_token }
+  let(:valid_user_headers) {
     {
       'Content-Type': 'application/json',
       'access-token': user_headers['access-token'],
       'uid': user_headers['uid'],
       'client': user_headers['client']
+    }
   }
-}
+
+  let(:valid_admin_headers) {
+    {
+      'Content-Type': 'application/json',
+      'access-token': admin_headers['access-token'],
+      'uid': admin_headers['uid'],
+      'client': admin_headers['client']
+    }
+  }
 
   describe "GET /index" do
-    it "renders a successful response" do
-      get api_v1_bookings_url, headers: valid_headers, as: :json
+    it "renders a successful response for admin" do
+      get api_v1_bookings_url, headers: valid_admin_headers, as: :json
       expect(response).to be_successful
+    end
+
+    it "renders a successful response for user" do
+      get api_v1_bookings_url, headers: valid_user_headers, as: :json
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 
   describe "GET /show" do
-    it "renders a successful response" do
+
+    it "renders a successful response for user" do
       booking = create(:api_v1_booking, user: user, tour: tour)
-      get api_v1_booking_url(booking), as: :json
+      get api_v1_booking_url(booking), headers: valid_user_headers, as: :json
       expect(response).to be_successful
-    end
   end
 
   describe "POST /create" do
     context "with valid parameters" do
-      it "creates a new Api::V1::Booking" do
+      it "creates a new Api::V1::Booking for user" do
         expect {
           post api_v1_bookings_url,
-               params: { api_v1_booking: valid_attributes }, headers: valid_headers, as: :json
+               params: { api_v1_booking: valid_attributes }, headers: valid_user_headers, as: :json
         }.to change(Api::V1::Booking, :count).by(1)
       end
 
-      it "renders a JSON response with the new api_v1_booking" do
+      it "renders a JSON response with the new api_v1_booking for user" do
         post api_v1_bookings_url,
-             params: { api_v1_booking: valid_attributes }, headers: valid_headers, as: :json
+             params: { api_v1_booking: valid_attributes }, headers: valid_user_headers, as: :json
         expect(response).to have_http_status(:created)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
+
     end
 
     context "with invalid parameters" do
@@ -77,7 +90,7 @@ RSpec.describe "/api/v1/bookings", type: :request do
 
       it "renders a JSON response with errors for the new api_v1_booking" do
         post api_v1_bookings_url,
-             params: { api_v1_booking: invalid_attributes }, headers: valid_headers, as: :json
+             params: { api_v1_booking: invalid_attributes }, headers: valid_user_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
@@ -86,34 +99,36 @@ RSpec.describe "/api/v1/bookings", type: :request do
 
   describe "PATCH /update" do
     context "with valid parameters" do
-      let(:new_attributes) {
+      let(:new_attributes){
         {
           booking_status: "confirmed"
     }
       }
 
-      it "updates the requested api_v1_booking" do
+
+      it "updates the requested api_v1_booking for admin" do
         booking = create(:api_v1_booking, user: user, tour: tour)
         patch api_v1_booking_url(booking),
-              params: { api_v1_booking: new_attributes }, headers: valid_headers, as: :json
+              params: { api_v1_booking: new_attributes }, headers: valid_admin_headers, as: :json
         booking.reload
         expect(booking.booking_status).to eq("confirmed")
       end
 
-      it "renders a JSON response with the api_v1_booking" do
+      it "renders a JSON response with the api_v1_booking for admin" do
         booking = create(:api_v1_booking, user: user, tour: tour)
         patch api_v1_booking_url(booking),
-              params: { api_v1_booking: new_attributes }, headers: valid_headers, as: :json
+              params: { api_v1_booking: new_attributes }, headers: valid_admin_headers, as: :json
         expect(response).to have_http_status(:ok)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
     end
 
     context "with invalid parameters" do
-      it "renders a JSON response with errors for the api_v1_booking" do
+
+      it "renders a JSON response with errors for the api_v1_booking for admin" do
         booking = create(:api_v1_booking, user: user, tour: tour)
         patch api_v1_booking_url(booking),
-              params: { api_v1_booking: invalid_attributes }, headers: valid_headers, as: :json
+              params: { api_v1_booking: invalid_attributes }, headers: valid_admin_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
@@ -121,11 +136,13 @@ RSpec.describe "/api/v1/bookings", type: :request do
   end
 
   describe "DELETE /destroy" do
-    it "destroys the requested api_v1_booking" do
+    it "destroys the requested api_v1_booking for admin" do
       booking = create(:api_v1_booking, user: user, tour: tour)
       expect {
-        delete api_v1_booking_url(booking), headers: valid_headers, as: :json
+        delete api_v1_booking_url(booking), headers: valid_admin_headers, as: :json
       }.to change(Api::V1::Booking, :count).by(-1)
     end
   end
+
+end
 end
